72a0cdebf5cfcb22637e527a8347e57754ea109b
Marc <marc@aguardio.com>
2023-04-24 10:10:24 +0200

    staging

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-// Import dotenv and load environment variables from .env file
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-
-
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
